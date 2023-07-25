bb697c1601eb5bccfdabcd36cd327e3368b055f4
tazrog <4278089+tazrog@users.noreply.github.com>
2023-07-24 19:37:16 -0700

    update playter3 move

---
+++ 2600basic_variable_redefs.h
@@ -0,0 +1,345 @@
+; This file contains variable mapping and other information for the current project.
+
+sc1 = score
+ 
+PlayerHealth = e
+ 
+splitKernelVar = r
+ 
+swdebounce = v
+ 
+gamenumber = u
+ 
+PlayerDamage = m
+ 
+Bit0_NewLevel = z
+ 
+Bit5_hit = z
+ 
+Ch0_Counter = y
+ 
+Ch0_Duration = x
+ 
+Ch0_Sound = w
+ 
+V0 = v
+ 
+Ch1_Counter = p
+ 
+Ch1_Duration = k
+ 
+Ch1_Sound = j
+ 
+V1 = h
+ 
+LEdge = s
+ 
+REdge = g
+ 
+Bit4_gameover = z
+ 
+Timer = t
+ 
+Househit = q
+ 
+Bit3_ShootorNot = z
+ 
+delay = o
+ 
+HealthDrop = n
+ 
+Bit1_missleOn = z
+ 
+level = l
+ 
+Bit2_EnemyMove = z
+ 
+frame = f
+ 
+Points = d
+ 
+EnemySpeed = c
+ 
+Moverate = b
+ 
+drop = a
+ 
+EnemyHit = i
+ 
+SPLIT_KERN_BIT = BIT_7
+ 
+splitscore_2_4 = 1
+ 
+_P_Edge_Right = 150
+ 
+_P_Edge_Left = 0
+ 
+_P_Edge_Bottom = 160
+ 
+_P_Edge_Top = 9
+ 
+bscode_length = 32
+_FE = $FE
+ 
+_FC = $FC
+ 
+_FA = $FA
+ 
+_F8 = $F8
+ 
+_F6 = $F6
+ 
+_F4 = $F4
+ 
+_F2 = $F2
+ 
+_F0 = $F0
+ 
+_EE = $EE
+ 
+_EC = $EC
+ 
+_EA = $EA
+ 
+_E8 = $E8
+ 
+_E6 = $E6
+ 
+_E4 = $E4
+ 
+_E2 = $E2
+ 
+_E0 = $E0
+ 
+_DE = $DE
+ 
+_DC = $DC
+ 
+_DA = $DA
+ 
+_D8 = $D8
+ 
+_D6 = $D6
+ 
+_D4 = $D4
+ 
+_D2 = $D2
+ 
+_D0 = $D0
+ 
+_CE = $CE
+ 
+_CC = $CC
+ 
+_CA = $CA
+ 
+_C8 = $C8
+ 
+_C6 = $C6
+ 
+_C4 = $C4
+ 
+_C2 = $C2
+ 
+_C0 = $C0
+ 
+_BE = $BE
+ 
+_BC = $BC
+ 
+_BA = $BA
+ 
+_B8 = $B8
+ 
+_B6 = $B6
+ 
+_B4 = $B4
+ 
+_B2 = $B2
+ 
+_B0 = $B0
+ 
+_AE = $AE
+ 
+_AC = $AC
+ 
+_AA = $AA
+ 
+_A8 = $A8
+ 
+_A6 = $A6
+ 
+_A4 = $A4
+ 
+_A2 = $A2
+ 
+_A0 = $A0
+ 
+_9E = $9E
+ 
+_9C = $9C
+ 
+_9A = $9A
+ 
+_98 = $98
+ 
+_96 = $96
+ 
+_94 = $94
+ 
+_92 = $92
+ 
+_90 = $90
+ 
+_8E = $8E
+ 
+_8C = $8C
+ 
+_8A = $8A
+ 
+_88 = $88
+ 
+_86 = $86
+ 
+_84 = $84
+ 
+_82 = $82
+ 
+_80 = $80
+ 
+_7E = $7E
+ 
+_7C = $7C
+ 
+_7A = $7A
+ 
+_78 = $78
+ 
+_76 = $76
+ 
+_74 = $74
+ 
+_72 = $72
+ 
+_70 = $70
+ 
+_6E = $6E
+ 
+_6C = $6C
+ 
+_6A = $6A
+ 
+_68 = $68
+ 
+_66 = $66
+ 
+_64 = $64
+ 
+_62 = $62
+ 
+_60 = $60
+ 
+_5E = $5E
+ 
+_5C = $5C
+ 
+_5A = $5A
+ 
+_58 = $58
+ 
+_56 = $56
+ 
+_54 = $54
+ 
+_52 = $52
+ 
+_50 = $50
+ 
+_4E = $4E
+ 
+_4C = $4C
+ 
+_4A = $4A
+ 
+_48 = $48
+ 
+_46 = $46
+ 
+_44 = $44
+ 
+_42 = $42
+ 
+_40 = $40
+ 
+_3E = $3E
+ 
+_3C = $3C
+ 
+_3A = $3A
+ 
+_38 = $38
+ 
+_36 = $36
+ 
+_34 = $34
+ 
+_32 = $32
+ 
+_30 = $30
+ 
+_2E = $2E
+ 
+_2C = $2C
+ 
+_2A = $2A
+ 
+_28 = $28
+ 
+_26 = $26
+ 
+_24 = $24
+ 
+_22 = $22
+ 
+_20 = $20
+ 
+_1E = $1E
+ 
+_1C = $1C
+ 
+_1A = $1A
+ 
+_18 = $18
+ 
+_16 = $16
+ 
+_14 = $14
+ 
+_12 = $12
+ 
+_10 = $10
+ 
+_0E = $0E
+ 
+_0C = $0C
+ 
+_0A = $0A
+ 
+_08 = $08
+ 
+_06 = $06
+ 
+_04 = $04
+ 
+_02 = $02
+ 
+_00 = $00
+ 
+font = retroputer
+ 
+vblank_time = 43
+overscan_time = 37
+bs_mask = 7
+bankswitch = 28
+bankswitch_hotspot = $1FF6
+multisprite = 2
